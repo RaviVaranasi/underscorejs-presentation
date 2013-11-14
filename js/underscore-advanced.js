aeb54e8a85cee544acffcbe99938d70a04b1a31e
Ravi Varanasi <r_varanasi@containerstore.com>
2013-11-13 21:40:37 -0600

    Added chain and merge

---
+++ js/underscore-advanced.js
@@ -0,0 +1,69 @@
+var _ = require('./libs/underscore');
+
+var orders = [
+	{
+	  "orderNumber": "1",
+      "orderDate": "2013-02-08",
+      "status": "IN_PROCESS",
+      "atHomeInd": true,
+	      "orderTotal": 20,
+	      "customerId": 2
+	},
+	{
+      "orderNumber": "2",
+      "orderDate": "2013-06-06",
+      "status": "PROCESSED",
+      "atHomeInd": false,
+      "orderTotal": 10,
+      "customerId": 2
+
+    },
+    {
+      "orderNumber": "3",
+      "orderDate": "2013-06-06",
+      "status": "IN_PROCESS",
+      "orderDescription": "Ship",
+      "atHomeInd": false,
+      "orderTotal": 50,
+      "customerId": 3
+
+    },
+     {
+      "orderNumber": "4",
+      "orderDate": "2013-09-09",
+      "orderAmount": "40",
+      "atHomeInd": true,
+	      "orderTotal": 100,
+      "customerId": 1
+    }
+];
+
+var customers = [
+	{
+	  "customerId": 1,
+      "firstName": "Mickey",
+      "lastName": "Mouse",
+      "customerType": 'WEB'
+	},
+	{
+	  "customerId": 2,
+      "firstName": "Donald",
+      "lastName": "Duck",
+      "customerType": 'WEB'
+	},
+	{
+	  "customerId": 3,
+      "firstName": "Buzz",
+      "lastName": "Lightyear",
+      "customerType": 'WEB'
+	}
+];
+
+var ordersWithCustomerName = _.map(orders, function(order){
+	var customer = _.findWhere(customers, {'customerId': order.customerId});
+	return _.extend(order, customer);
+});
+console.log(ordersWithCustomerName);
+
+
+
---
+++ js/underscore-chainandcompose.js
@@ -0,0 +1,58 @@
+var _ = require('./libs/underscore');
+
+
+var wallModels = [
+	{
+		startX: 20, startY: 18, 
+		endX: 92, endY: 18, 
+		wallId: 'A'
+	},
+    {
+    	startX: 92, startY: 18, 
+    	endX: 92, endY: 54, 
+    	wallId: 'B'
+    },
+	{
+		startX: 92, startY: 54, 
+		endX: 20, endY: 54, 
+		wallId: 'C',
+		doors :[{
+			doorType: 'standard',
+			isPrimary: true
+		}]
+	},
+	{
+		startX: 20, startY: 54, 
+		endX: 20, endY: 18, 
+		wallId: 'D',
+		doors :[{
+			doorType: 'standard',
+			isPrimary: false
+		}]
+	}
+];
+
+
+// GET THE PRIMARY door
+// var allDoorsIncludesNull = _.map(wallModels, function(wallModel){
+// 	return wallModel.doors;
+// });
+// var allDoors =  _.compact(_.flatten(allDoorsIncludesNull));
+// console.log('all doors %j', allDoors);
+// var primaryDoor = _.findWhere(allDoors, {isPrimary: true});
+// console.log('primary door %j', primaryDoor);
+
+//REFACTOR start
+var doorModels = function(wallModel){
+	return wallModel.doors;
+};
+var isPrimary = function(door){
+	return door.isPrimary;
+};
+console.log(_.chain(wallModels)
+	.map(doorModels)
+	.flatten()
+	.compact()
+	// .tap(console.log)
+	.filter(isPrimary)
+	.value());
